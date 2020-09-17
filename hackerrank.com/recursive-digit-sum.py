4e1aa41f276629dee6b5c2678002d4f216374301
Kamil Pajdzik <pajdziu@live.com>
2020-09-16 22:42:50 -0700

    Improve solution for "Recursive Digit Sum"

--- hackerrank.com/recursive-digit-sum.py
+++ hackerrank.com/recursive-digit-sum.py
@@ -12,24 +12,21 @@ def digits_sum(n):
     if n // 10 == 0:
         return n
 
-    digits = []
+    sum_of_digits = 0
     while n > 0:
-        digits.append(n % 10)
+        sum_of_digits += (n % 10)
         n = n // 10
 
-    return digits_sum(sum(digits))
+    return digits_sum(sum_of_digits)
 
 def superDigit(n, k):
-    number = int(str(n) * k)
-    return digits_sum(number)
+    # proof: http://applet-magic.com/digitsummod9.htm
+    res = ((n % 9) * (k % 9)) % 9
+    return res or 9
 
 if __name__ == '__main__':
     nk = input().split()
-
-    n = nk[0]
-
+    n = int(nk[0])
     k = int(nk[1])
-
     result = superDigit(n, k)
-
     print(str(result) + '\n')
