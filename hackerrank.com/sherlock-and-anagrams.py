fbb702632025651f141665453fc524744b137d89
Kamil Pajdzik <pajdziu@live.com>
2020-12-02 20:32:52 -0800

    Wrong solution for "Sherlock and anagrams"

--- hackerrank.com/sherlock-and-anagrams.py
+++ hackerrank.com/sherlock-and-anagrams.py
@@ -9,19 +9,35 @@ import sys
 
 # Complete the sherlockAndAnagrams function below.
 def sherlockAndAnagrams(s):
-    duplicates = { }
-    for c in s:
-        duplicates[c] = 1 if c not in duplicates else duplicates[c] + 1
+    count = 0
 
-    dup_chars = [c for c in duplicates.keys() if duplicates[c] > 1]
+    for i in range(len(s)):
+        for j in range(len(s) - 1, i, -1):
+            if s[i] == s[j]:
+                count += countAnagrams(s, i, j)
 
-    if (any(dup_chars)):
-        return 0
+    return count
 
-    
+def countAnagrams(s, i, j):
+    c = 0
+    si = set()
+    sj = set()
+
+    while i < j and si == sj:
+        si.add(i)
+        sj.add(j)
+        i += 1
+        j -= 1
+
+        if si != sj:
+            c += 1
+
+        print(str([s[x] for x in si]) + " | " + str([s[x] for x in sj]))
+
+    return c
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
     q = int(input())
 
@@ -29,7 +45,4 @@ if __name__ == '__main__':
         s = input()
 
         result = sherlockAndAnagrams(s)
-
-        fptr.write(str(result) + '\n')
-
-    fptr.close()
+        print(str(result) + '\n')
