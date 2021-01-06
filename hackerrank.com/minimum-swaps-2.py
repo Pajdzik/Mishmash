1599abc70666d740c1bc4d42c88ebf298c88eedf
Kamil Pajdzik <pajdziupaj@gmail.com>
2021-01-05 22:31:49 -0800

    Add solution for "Minimum swaps 2"

--- hackerrank.com/minimum-swaps-2.py
+++ hackerrank.com/minimum-swaps-2.py
@@ -8,8 +8,9 @@ import re
 import sys
 
 # Complete the minimumSwaps function below.
-def minimumSwaps(arr):
+def minimumSwaps_polynomial(arr):
     count = 0
+    i = 0
 
     for i in range(len(arr)):
         if arr[i] != i + 1:
@@ -20,16 +21,27 @@ def minimumSwaps(arr):
 
     return count
 
+def minimumSwaps(arr):
+    count = 0
+    i = 0
+
+    while i < len(arr):
+        if arr[i] == i + 1:
+            i += 1
+            continue
+        
+        t = arr[arr[i] - 1]
+        arr[arr[i] - 1] = arr[i]
+        arr[i] = t
+        count += 1
+
+    return count
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    n = int(input())
-
-    arr = list(map(int, input().rstrip().split()))
-
+    #fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    #n = int(input())
+    arr = [1, 3, 2, 4] #list(map(int, input().rstrip().split()))
     res = minimumSwaps(arr)
-
-    fptr.write(str(res) + '\n')
-
-    fptr.close()
+    #fptr.write(str(res) + '\n')
+    #fptr.close()
