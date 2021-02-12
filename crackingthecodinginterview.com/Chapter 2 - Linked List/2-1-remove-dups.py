c488d81304afc2903fa82f25e552821b13a5c101
Kamil Pajdzik <pajdziu@live.com>
2021-02-11 19:46:40 -0800

    Add alternative solution for "Remove dups"

--- crackingthecodinginterview.com/Chapter 2 - Linked List/2-1-remove-dups.py
+++ crackingthecodinginterview.com/Chapter 2 - Linked List/2-1-remove-dups.py
@@ -3,7 +3,7 @@
 
 from linkedlist import new_linked_list, print_linked_list
 
-def remove_dups(root_node):
+def remove_dups_with_buffer(root_node):
     visited = { root_node.value }
     prev_node = root_node
     curr_node = root_node.next
@@ -11,11 +11,30 @@ def remove_dups(root_node):
     while curr_node:
         if curr_node.value in visited:
             prev_node.next = curr_node.next
-            curr_node = curr_node.next
-            continue
+        else:
+            visited.add(curr_node.value)
+            prev_node = curr_node
 
-        prev_node = curr_node
         curr_node = curr_node.next
+
+    return root_node
+
+def remove_dups(root_node):
+    outer_node = root_node
+    
+    while outer_node:
+        prev_node = outer_node
+        inner_node = outer_node.next
+
+        while inner_node:
+            if outer_node.value == inner_node.value:
+                prev_node.next = inner_node.next
+            else:
+                prev_node = inner_node
+                
+            inner_node = inner_node.next
+
+        outer_node = outer_node.next
 
     return root_node
 
@@ -36,4 +55,7 @@ if __name__ == "__main__":
     run_and_print(input_root)
 
     input_root = new_linked_list([1, 2, 1, 1])
+    run_and_print(input_root)
+
+    input_root = new_linked_list([1, 2, 2, 1])
     run_and_print(input_root)
