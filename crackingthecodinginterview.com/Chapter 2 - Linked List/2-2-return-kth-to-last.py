172517f1b88b1bb5ec274bb40898b9c565c48ad3
Kamil Pajdzik <pajdziu@live.com>
2021-02-11 21:33:35 -0800

    Add solution for "Reverse linked list" problem

--- crackingthecodinginterview.com/Chapter 2 - Linked List/2-2-return-kth-to-last.py
+++ crackingthecodinginterview.com/Chapter 2 - Linked List/2-2-return-kth-to-last.py
@@ -3,20 +3,44 @@
 
 from linkedlist import new_linked_list, print_linked_list
 
-def return_kth_to_last(node, k):
+def get_length(node):
+    length = 0
+
     while node:
-        if k == 1:
+        length += 1
+        node = node.next
+
+    return length
+
+def return_kth_to_last_calc_length(node, k):
+    length = get_length(node)
+    n = length - k
+
+    while node:
+        if n == 1:
             return node.value
 
-        k -= 1
+        n -= 1
         node = node.next
 
     return None
 
+def return_kth_to_last(node, k, counter):
+    if node is None:
+        return None
+
+    counter += 1
+    result_node = return_kth_to_last(node.next, k, counter)
+
+    if counter == k:
+        return node
+
+    return result_node
+
 def run_and_print(root_node, k):
     print_linked_list(root_node)
-    result = return_kth_to_last(root_node, k)
-    print("{}th element: {}".format(k, result))
+    result = return_kth_to_last(root_node, k, 0)
+    print("{}th to last element: {}".format(k, result and result.value))
     print("")
 
 if __name__ == "__main__":
@@ -24,7 +48,7 @@ if __name__ == "__main__":
     run_and_print(input_root, 1)
 
     input_root = new_linked_list([1, 1, 3, 1])
-    run_and_print(input_root, 4)
+    run_and_print(input_root, 2)
     
     input_root = new_linked_list([1, 1, 3, 1])
     run_and_print(input_root, 3)
