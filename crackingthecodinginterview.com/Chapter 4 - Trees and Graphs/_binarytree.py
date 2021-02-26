50ff35d58aea56ea7c814e128f6c2361c2c2d32e
Kamil Pajdzik <kamip@microsoft.com>
2021-02-25 19:10:41 -0800

    Add binary tree draft

---
+++ crackingthecodinginterview.com/Chapter 4 - Trees and Graphs/_binarytree.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
+
+def add_node(node, value):
+    # if (value > node.value):
+
+
+def create_binary_tree(nodes):
+    root_node = nodes[0]
+
+    for node in nodes[1:]:
+
+
+if __name__ == "__main__":
