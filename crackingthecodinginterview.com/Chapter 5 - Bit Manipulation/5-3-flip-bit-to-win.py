550a84928ba186cdb38c2f436128857d0742f492
Kamil Pajdzik <pajdziu@live.com>
2021-03-24 21:42:50 -0700

    Add solution for "Flip bit to win" problem

---
+++ crackingthecodinginterview.com/Chapter 5 - Bit Manipulation/5-3-flip-bit-to-win.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+# You have an integer and you can flip exactly one bit from a 0 to a 1,
+# write code to find the length of the longest sequence of 1s you could create.
+
+from _bit import print_binary
+
+def find_sequences(n):
+    current_value = 0
+    current_length = 0
+    sequences = []
+
+    for bit in range(32, -1, -1):
+        if (n & (1 << bit)) == (current_value << bit):
+            current_length += 1
+        else:
+            sequences.append(current_length)
+            current_value = 1 if current_value == 0 else 0
+            current_length = 1
+
+    return sequences
+
+def flip_bit_to_win(n):
+    sequences = find_sequences(n)
+    max_length = 0
+
+    for i in range(1, len(sequences), 2):
+        if ((i + 1 < len(sequences)) and sequences[i + 1] == 1) \
+          and ((i + 2 < len(sequences)) and sequences[i] + sequences[i + 2] > max_length):
+            max_length = sequences[i] + sequences[i + 2]
+        if sequences[i] > max_length:
+            max_length = sequences[i]
+
+    return max_length
+
+if __name__ == "__main__":
+    n = 0xAE9B
+    print_binary(n)
+    print(flip_bit_to_win(n))
+    pass
