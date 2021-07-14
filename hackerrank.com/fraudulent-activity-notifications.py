a4b79cea83d42d861b7ebada1dfbb7b1e80804b7
Kamil Pajdzik <pajdziu@live.com>
2021-07-13 23:15:10 -0700

    Improve solution for "Fraudulent activity notificatinos"

--- hackerrank.com/fraudulent-activity-notifications.py
+++ hackerrank.com/fraudulent-activity-notifications.py
@@ -1,29 +1,68 @@
 #!/bin/python3
 #https://www.hackerrank.com/challenges/fraudulent-activity-notifications
 
-import math
-import os
-import random
-import re
-import sys
+def calculate_double_median(value_count, length):
+    median_i, median_j = None, None
+    
+    median_index = (length // 2) + 1
+    if length % 2 != 0:
+        median_i, median_j = median_index, median_index
+    else:
+        median_i, median_j = median_index - 1, median_index
 
-def median(arr):
-    data = sorted(arr)
-    l = len(data)
-    l2 = int(l / 2)
-    med = ((data[l2] + data[l2 - 1]) / 2.0) if l % 2 == 0 else data[l2]
-    return med
+    i = 0
+    median_low, median_high = None, None
 
-# Quickselect
+    for value in range(len(value_count)):
+        if median_high != None:
+            break
+
+        for _ in range(value_count[value]):
+            i += 1
+            if i == median_i:
+                median_low = value
+            if i == median_j:
+                median_high = value 
+
+    return median_low + median_high
+
+def count_elements(arr, count_lenght):
+    value_count = [0 for _ in range(count_lenght)]
+
+    for i in arr:
+        value_count[i] += 1  
+
+    return value_count
+
+def counting_sort(arr):
+    value_count = count_elements(arr)
+
+    output = [0 for _ in range(len(arr))]
+    index = 0
+    for value in range(len(value_count)):
+        for _ in range(value_count[value]):
+            output[index] = value
+            index += 1
+
+    return output
+
 
 # Complete the activityNotifications function below.
 def activityNotifications(expenditure, d):
     c = 0
+    value_count = count_elements(expenditure[:d], max(expenditure) + 1)
 
     for i in range(d, len(expenditure)):
-        med = median(expenditure[i - d:i])
-        if expenditure[i] >= 2 * med:
+        double_median = calculate_double_median(value_count, d)
+
+        last_element = expenditure[i]
+        if last_element >= double_median:
             c += 1
+
+        first_element = expenditure[i - d]
+
+        value_count[first_element] -= 1
+        value_count[last_element] += 1
 
     return c
 
