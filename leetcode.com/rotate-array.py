2562f153f09a83ccfc67323a94558ff1ae586a25
Kamil Pajdzik <pajdziu@live.com>
2021-07-29 22:39:19 -0700

    Add solution for "Rotate array" problem

--- leetcode.com/rotate-array.py
+++ leetcode.com/rotate-array.py
@@ -1,17 +1,17 @@
 #!/bin/python3
 # https://leetcode.com/problems/rotate-array/
 
+def flip(nums: list[int], start: int, end: int) -> None:
+    while start < end:
+        nums[start], nums[end] = nums[end], nums[start]
+        start += 1
+        end -= 1
+
 class Solution:
     def rotate(self, nums: list[int], k: int) -> None:
-        for _ in range(0, k):
-            previous_value = nums[0]
-            for i in range(1, len(nums)):
-                temp = nums[i]
-                nums[i] = previous_value
-                previous_value = temp
-
-            nums[0] = previous_value
-
-        pass
+        k = k % len(nums)
+        flip(nums, 0, len(nums) - 1)
+        flip(nums, 0, k - 1)
+        flip(nums, k, len(nums) - 1)
 
 Solution().rotate([1, 2, 3, 4, 5, 6, 7], 3)
