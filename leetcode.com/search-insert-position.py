c05acc0ef1790d73aa98cbd1d40b45d160c16ab6
Kamil Pajdzik <pajdziu@live.com>
2021-08-08 14:42:52 -0700

    Add solution for "Search Insert Position" problem

---
+++ leetcode.com/search-insert-position.py
@@ -0,0 +1,21 @@
+def binary_search(nums: list[int], left: int, right: int, target: int) -> int:
+    middle = max(left + ((right - left) // 2), 0)
+    element = nums[middle]
+
+    if element == target:
+        return middle
+    elif right <= left:
+        if target > element:
+            return middle + 1
+        else:
+            return middle
+    elif target > element:
+        return binary_search(nums, middle + 1, right, target)
+    else:
+        return binary_search(nums, left, middle - 1, target)
+
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        return binary_search(nums, 0, len(nums) - 1, target)
+
+Solution().searchInsert([1,3], 0)
