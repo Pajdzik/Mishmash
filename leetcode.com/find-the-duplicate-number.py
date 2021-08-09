616bb86ada30b85a6b4833f74697ab70a5b938fd
Kamil Pajdzik <pajdziu@live.com>
2021-08-08 20:32:27 -0700

    Add solution for "Find the Duplicate Number" problem

--- leetcode.com/find-the-duplicate-number.py
+++ leetcode.com/find-the-duplicate-number.py
@@ -2,7 +2,26 @@
 # https://leetcode.com/problems/find-the-duplicate-number/
 
 class Solution:
-    def findDuplicate(self, nums: list[int]) -> int:
+    def findDuplicate_bst(self, nums: list[int]) -> int:
+        low = 1
+        high = len(nums) - 1
+        
+        while low < high:
+            middle = low + ((high - low) // 2)
+            count = 0
+
+            for num in nums:
+                if num <= middle:
+                    count += 1
+
+            if count > middle:
+                high = middle
+            else:
+                low = middle + 1
+
+        return low
+
+    def findDuplicate_map(self, nums: list[int]) -> int:
         m = len(nums)
         visited = [ False for _ in range(m) ]
 
