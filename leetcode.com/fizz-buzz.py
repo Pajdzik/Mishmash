ee0ad39f9d5a32c1e2d865357c3b68336577e1b1
Kamil Pajdzik <pajdziu@live.com>
2021-09-01 21:32:07 -0700

    Add solution for "Fizz Buzz" problem

---
+++ leetcode.com/fizz-buzz.py
@@ -0,0 +1,17 @@
+#!/bin/python3
+# https://leetcode.com/problems/fizz-buzz/submissions/
+
+class Solution:
+    def fizzBuzz(self, n: int) -> list[str]:
+        result = []
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                result.append("FizzBuzz")
+            elif i % 3 == 0:
+                result.append("Fizz")
+            elif i % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(i))
+
+        return result
