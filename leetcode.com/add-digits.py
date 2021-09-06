826475de7d17e72aa1e7d9c5ca7ec0a4f97f8fe0
Kamil Pajdzik <pajdziu@live.com>
2021-09-05 22:44:07 -0700

    Add solution for "Add Digits" problem

---
+++ leetcode.com/add-digits.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+# https://leetcode.com/problems/add-digits/
+
+class Solution:
+    def addDigits(self, num: int) -> int:
+        while num > 9:
+            sum = 0
+            while num > 0:
+                sum += num % 10
+                num //= 10
+            
+            num = sum
+        
+        return num
