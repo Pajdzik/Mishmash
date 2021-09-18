099450a108f835f49bae32559eb8bd5f0b873764
Kamil Pajdzik <pajdziupaj@gmail.com>
2021-09-17 21:03:26 -0700

    Add solution for "Min Stack" problem

--- leetcode.com/min-stack.py
+++ leetcode.com/min-stack.py
@@ -7,36 +7,47 @@ class MinStack:
         initialize your data structure here.
         """
         self.stack = []
-        self.min_queue = []
-        
+        self.min = None
+        self.mins = []
 
     def push(self, val: int) -> None:
         self.stack.append(val)
-        i = 0
-        while i < len(self.min_queue) and val > self.min_queue[i]:
-            i += 1
-        self.min_queue.insert(i, val)
+
+        if self.min == None or self.min >= val:
+            self.min = val
+            self.mins.append(val)
         
     def pop(self) -> None:
         val = self.stack.pop()
-        i = 1
-        for i in range(len(self.min_queue)):
-            if self.min_queue[i] == val:
-                break
-        self.min_queue.pop(i)
+
+        if len(self.stack) == 0:
+            self.min = None
+        elif val == self.min:
+            self.mins.pop()
+            self.min = self.mins[-1]
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return self.min_queue[0]
+        return self.mins[-1]
         
 minStack = MinStack();
-minStack.push(-2);
-minStack.push(0);
-minStack.push(-3);
-val1 = minStack.getMin(); 
-minStack.pop();
-val2 = minStack.top();    
-val3 = minStack.getMin();
+minStack.push(2147483646)
+minStack.push(2147483646)
+minStack.push(2147483647)
+val1 = minStack.top()
+minStack.pop()
+val2 = minStack.getMin()
+minStack.pop()
+val3 = minStack.getMin()
+minStack.pop()
+minStack.push(2147483647)
+val4 = minStack.top()
+minStack.getMin()
+minStack.push(-2147483648)
+val5 = minStack.top()
+val6 = minStack.getMin()
+minStack.pop()
+val7 = minStack.getMin()
 pass
