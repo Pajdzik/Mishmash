f8cd8e97f0970e2671a672d4551e3a5b1b2a7a02
Kamil Pajdzik <pajdziu@live.com>
2021-09-23 19:51:01 -0700

    Add solution for "Two Sum" problem

--- leetcode.com/two-sum.py
+++ leetcode.com/two-sum.py
@@ -3,7 +3,21 @@
 
 class Solution:
     def twoSum(self, nums: list[int], target: int) -> list[int]:
+        cache = {}
+
+        for i in range(len(nums)):
+            num = nums[i]
+            diff = target - num
+
+            if diff in cache:
+                return [ cache[diff], i ]
+
+            cache[num] = i
+
+    def twoSum_bruteforce(self, nums: list[int], target: int) -> list[int]:
         for i in range(0, len(nums) - 1):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+
+Solution().twoSum([3,2,4], 6)
