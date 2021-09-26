90ccf8e68c332939ea414749e010b9437253446e
Kamil Pajdzik <pajdziu@live.com>
2021-09-26 14:53:40 -0700

    Add solution for "3Sum" problem

--- leetcode.com/3sum.py
+++ leetcode.com/3sum.py
@@ -3,21 +3,25 @@
 
 class Solution:
     def threeSum(self, nums: list[int]) -> list[list[int]]:
-        result = []
-
-        for i in range(0, len(nums) - 1):
-            target = -nums[i]
-            cache = set()
-
+        result = set()
+        seen = {}
+        
+        for i in range(len(nums)):
+            a = nums[i]
+            if a not in seen:
+                seen[a] = i
+            
             for j in range(i + 1, len(nums)):
-                value = target - nums[j]
-                if value in cache:
-                    sorted_result = sorted([nums[i], nums[j], value])
-                    if sorted_result not in result:
-                        result.append(sorted_result)
-                else:
-                    cache.add(nums[j])
-
-        return result
+                b = nums[j]
+                c = -(nums[i] + nums[j])
+                
+                if c in seen and seen[c] != i != j:
+                    result_three = tuple(sorted((a, b, c)))
+                    if not result_three in result and i != j != seen[c]:
+                        result.add(result_three)
+                
+                seen[b] = j
+                
+        return list(result)
 
 Solution().threeSum([-1,0,1,2,-1,-4])
