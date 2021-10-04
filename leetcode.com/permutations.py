e6ee1d9bb0aa8475b466e5f785531f3a4842665e
Kamil Pajdzik <pajdziu@live.com>
2021-10-03 22:35:56 -0700

    Add solution for "Permutations" problem

--- leetcode.com/permutations.py
+++ leetcode.com/permutations.py
@@ -1,24 +1,37 @@
 #!/bin/python3
 # https://leetcode.com/problems/permutations/
 
-def generate_permutation(index: int, nums: list[int], permutations: list[list[int]]):
+def generate_permutation_heap(index: int, nums: list[int], permutations: list[list[int]]):
     if index == 1:
         permutations.append(nums.copy())
     else:
-        generate_permutation(index - 1, nums, permutations)
+        generate_permutation_heap(index - 1, nums, permutations)
         for i in range(0, index - 1):
             if index % 2 == 0:
                 nums[i], nums[index - 1] = nums[index - 1], nums[i]
             else:
                 nums[0], nums[index - 1] = nums[index - 1], nums[0]
 
-            generate_permutation(index - 1, nums, permutations)
+            generate_permutation_heap(index - 1, nums, permutations)
 
 class Solution:
+    def permute_recursively(self, nums: list[int], permutation: list[int], taken: list[bool], results: list[list[int]]) -> None:
+        if len(permutation) == len(nums):
+            results.append(permutation[:])
+        else:
+            for i, num in enumerate(nums):
+                if taken[i] == True:
+                    continue
+
+                taken[i] = True
+                permutation.append(num)
+                self.permute_recursively(nums, permutation, taken, results)
+                permutation.remove(num)
+                taken[i] = False
+
     def permute(self, nums: list[int]) -> list[list[int]]:
         permutations = []
-        generate_permutation(len(nums), nums, permutations)
-
+        self.permute_recursively(nums, [], [False] * len(nums), permutations)
         return permutations
 
 Solution().permute([1, 2, 3])
