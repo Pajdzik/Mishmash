0542b22958d3cebee2661fb8ead4e2521021ba75
Kamil Pajdzik <pajdziu@live.com>
2021-10-04 22:26:53 -0700

    Add solution for "Subsets" problem

--- leetcode.com/subsets.py
+++ leetcode.com/subsets.py
@@ -3,6 +3,34 @@
 
 class Solution:
     def subsets(self, nums: list[int]) -> list[list[int]]:
+        results = self.subsets_cascading(nums)
+        return results
+
+    def subsets_cascading(self, nums: list[int]) -> list[list[int]]:
+        index = 0
+        results = [[]]
+
+        while index < len(nums):
+            current_len = len(results)
+            for i in range(current_len):
+                results.append(results[i] + [nums[index]])
+
+            index += 1
+
+        return results
+
+    def subsets_recursive(self, nums: list[int], index: int, subset: list[int], results: list[list[int]]) -> None:
+        if index >= len(nums):
+            return
+
+        subset.append(nums[index])
+        results.append(subset[:])
+        self.subsets_recursive(nums, index + 1, subset, results)
+        subset.pop()
+        self.subsets_recursive(nums, index + 1, subset, results)
+
+    
+    def subsets_binarymask(self, nums: list[int]) -> list[list[int]]:
         count_of_subsets = 2**len(nums)
         subsets = []
 
