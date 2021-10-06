42a2f5a420cc898339b41227c2c67098687d587e
Kamil Pajdzik <pajdziu@live.com>
2021-10-05 19:01:16 -0700

    Add solution for "Populating Next Right Pointers in Each Node II" problem

--- leetcode.com/populating-next-right-pointers-in-each-node-ii.py
+++ leetcode.com/populating-next-right-pointers-in-each-node-ii.py
@@ -10,29 +10,25 @@ class Node:
         self.next = next
 
 class Solution:
-    def connect(self, root: Node) -> Node:
+    def connect(self, root: 'Node') -> 'Node':
         if not root:
             return None
 
         queue = [ root ]
+        while queue:
+            level_len = len(queue)
+            for i in range(0, level_len - 1):
+                queue[i].next = queue[i + 1]
 
-        while True:
-            level = []
-
-            while len(queue) > 0:
+            while level_len > 0:
                 node = queue.pop(0)
-
                 if node.left:
-                    level.append(node.left)
-                
+                    queue.append(node.left)
                 if node.right:
-                    level.append(node.right)
-
-            for i in range(0, len(level) - 1):
-                level[i].next = level[i + 1]
-
-            queue = level
-            if len(queue) == 0:
-                break
+                    queue.append(node.right)
+                level_len -= 1
 
         return root
+
+if __name__ == "__main__":
+    Solution().connect(Node(1, Node(2, Node(4), Node(5)), Node(3, None, Node(7))))
