ce045696bfd2ad7cab6e6e724b1f08e6014d4907
Kamil Pajdzik <pajdziu@live.com>
2021-10-12 16:36:21 -0700

    Add solution for "Delete Operation for Two Strings" problem

--- leetcode.com/delete-operation-for-two-strings.py
+++ leetcode.com/delete-operation-for-two-strings.py
@@ -4,6 +4,28 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         memory = [ [ None for _ in range(len(word2) + 1) ] for _ in range(len(word1) + 1) ]
+        def calculate_longest_common_subsequence(word1: str, i1: int, word2: str, i2: int) -> int:
+            if i1 == 0 or i2 == 0:
+                return 0
+
+            if memory[i1][i2] != None:
+                return memory[i1][i2]
+            
+            if word1[i1 - 1] == word2[i2 - 1]:
+                lcs = 1 + calculate_longest_common_subsequence(word1, i1 - 1, word2, i2 - 1)
+                memory[i1][i2] = lcs
+            else:
+                lcs1 = calculate_longest_common_subsequence(word1, i1 - 1, word2, i2)
+                lcs2 = calculate_longest_common_subsequence(word1, i1, word2, i2 - 1)
+                memory[i1][i2] = max(lcs1, lcs2)
+            
+            return memory[i1][i2]
+
+        lcs = calculate_longest_common_subsequence(word1, len(word1), word2, len(word2))
+        return len(word1) + len(word2) - 2 * lcs
+
+    def minDistance_dp(self, word1: str, word2: str) -> int:
+        memory = [ [ None for _ in range(len(word2) + 1) ] for _ in range(len(word1) + 1) ]
 
         for i in range(len(word1) + 1):
             memory[i][0] = 0
