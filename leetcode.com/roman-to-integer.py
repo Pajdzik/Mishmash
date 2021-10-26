16e39214d57152597d1be8b2e11c789324077d8b
Kamil Pajdzik <pajdziu@live.com>
2021-10-25 21:21:04 -0700

    Add solution for "Roman to Integer" problem

--- leetcode.com/roman-to-integer.py
+++ leetcode.com/roman-to-integer.py
@@ -35,4 +35,28 @@ class Solution:
 
         return result
 
+    def romanToInt2(self, s: str) -> int:
+        map = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        result = 0
+        
+        i = 0
+        while i < len(s):
+            if i + 1 < len(s) and map[s[i]] < map[s[i + 1]]:
+                result -= map[s[i]]
+            else:
+                result += map[s[i]]
+            i += 1
+                
+        return result
+            
+
 Solution().romanToInt("IV")
