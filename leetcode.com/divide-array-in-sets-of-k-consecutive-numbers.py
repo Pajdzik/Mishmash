70a447f01488f77c6ad1cb9c6d4f9c076515d1c5
Kamil Pajdzik <pajdziupaj@gmail.com>
2021-11-11 22:45:54 -0800

    Add solution for "Divide Array in Sets of K Consecutive Numbers" problem

--- leetcode.com/divide-array-in-sets-of-k-consecutive-numbers.py
+++ leetcode.com/divide-array-in-sets-of-k-consecutive-numbers.py
@@ -1,20 +1,20 @@
 #!/bin/python3
-# https://leetcode.com/problems/hand-of-straights/
+# https://leetcode.com/problems/divide-array-in-sets-of-k-consecutive-numbers
 
 from typing import Counter
 
 class Solution:
-    def isNStraightHand(self, hand: list[int], groupSize: int) -> bool:
-        if len(hand) % groupSize > 0:
+    def isPossibleDivide(self, nums: list[int], k: int) -> bool:
+        if len(nums) % k > 0:
             return False
 
-        counted_hand = Counter(hand)
+        counted_hand = Counter(nums)
         
-        for c in sorted(hand):
+        for c in sorted(nums):
             if counted_hand[c] == 0:
                 continue
 
-            for next in range(c, c + groupSize):
+            for next in range(c, c + k):
                 if next not in counted_hand or counted_hand[next] == 0:
                     return False
                 else:
