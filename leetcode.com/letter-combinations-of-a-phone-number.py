1fe488e182f33c5c5c910f4b127b2570ba6e276e
Kamil Pajdzik <pajdziu@live.com>
2021-11-13 12:13:11 -0800

    Add solution for "Letter Combinations of a Phone Number" problem

--- leetcode.com/letter-combinations-of-a-phone-number.py
+++ leetcode.com/letter-combinations-of-a-phone-number.py
@@ -17,6 +17,21 @@ class Solution:
         if len(digits) == 0:
             return []
 
+        result = phone_keyboard[digits[0]][:]
+        
+        for digit in digits[1:]:
+            number_len = len(result)
+            for _ in range(number_len):
+                number = result.pop(0)
+                for letter in phone_keyboard[digit]:
+                    result.append(number + letter)
+                    
+        return result
+        
+    def letterCombinations_1(self, digits: str) -> list[str]:
+        if len(digits) == 0:
+            return []
+
         combinations = phone_keyboard[digits[0]].copy()
 
         for digit in digits[1:]:
