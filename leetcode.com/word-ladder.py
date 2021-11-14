058cf2d0b82917e43539cf305c092ef1c66ae387
Kamil Pajdzik <pajdziu@live.com>
2021-11-13 22:31:05 -0800

    Add solution for "Word Ladder" problem

--- leetcode.com/word-ladder.py
+++ leetcode.com/word-ladder.py
@@ -5,31 +5,21 @@ from collections import deque
 
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: list[str]) -> int:
-        def is_diff_single_letter(word: str, other_word: str) -> bool:
-            diff_char = False
+        connections = { }
+
+        def get_word_with_wildcards(word: str) -> list[str]:
             for i in range(len(word)):
-                if word[i] != other_word[i]:
-                    if diff_char:
-                        return False
-                    else:
-                        diff_char = True
+                yield word[:i] + "*" + word[i + 1:]
 
-            return True
-
-        def add_connections(word: str, start_index: int = 0) -> None:
-            for j in range(start_index, len(wordList)):
-                other_word = wordList[j]
-                if is_diff_single_letter(word, other_word):
-                    connections[word].append(other_word)
-                    connections[other_word].append(word)
-
-        connections = { word: [] for word in wordList }
-        connections[beginWord] = []
-
-        add_connections(beginWord)
+        def add_word(word: str):
+            for wildcard_word in get_word_with_wildcards(word):
+                if wildcard_word not in connections:
+                    connections[wildcard_word] = []
+                
+                connections[wildcard_word].append(word)
 
         for i, word in enumerate(wordList):
-            add_connections(word, i + 1)
+            add_word(word)
 
         visited = set()
         queue = deque([ (beginWord, 1) ])
@@ -38,14 +28,18 @@ class Solution:
             node, distance = queue.popleft()
             visited.add(node)
 
-            if node == endWord:
-                return distance
-
-            for target_node in connections[node]:
-                if target_node in visited:
+            for wildcard_word in get_word_with_wildcards(node):
+                if wildcard_word not in connections:
                     continue
+                
+                for target_node in connections[wildcard_word]:
+                    if target_node in visited:
+                        continue
 
-                queue.append((target_node, distance + 1))
+                    if target_node == endWord:
+                        return distance + 1
+
+                    queue.append((target_node, distance + 1))
 
         return 0
 
