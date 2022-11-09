98475daba863bea49422ab39f1c9001b11bb0f3d
Kamil Pajdzik <pajdziupaj@gmail.com>
2022-11-08 17:19:59 -0800

    Add iterative solution to "Climbing stairs" problem

--- leetcode.com/climbing-stairs.rb
+++ leetcode.com/climbing-stairs.rb
@@ -25,3 +25,21 @@ def climb_stairs_cache(n)
 
     return count(n, cache)
 end
+
+def climb_stairs(n)
+    return 0 if n <= 0
+    return 1 if n == 1
+    return 2 if n == 2
+
+    count = 0
+    one_step = 1
+    two_steps = 2
+
+    (3..n).each {
+        puts "#{n}: #{count}"
+        count = one_step + two_steps
+        one_step, two_steps = two_steps, count
+    }
+
+    return count
+end
