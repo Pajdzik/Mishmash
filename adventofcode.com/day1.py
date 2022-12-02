ec263e5d1329a43ef21c8bc64cd1c6ac735c42c5
Kamil Pajdzik <pajdziupaj@gmail.com>
2022-12-01 20:57:36 -0800

    Add solution to Day 1 of Advent of Code

---
+++ adventofcode.com/day1.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+def find_max(input) -> int:
+    max_sum = 0
+    sum = 0
+
+    for line in input:
+        if line == "" or line == "\n":
+            max_sum = max(max_sum, sum)
+            sum = 0
+        else:
+            sum += int(line)
+
+    return max_sum
+                
+if __name__ == '__main__':
+    input = open('day1.txt', 'r')
+    max = find_max(input)
+    print(max)
+    input.close()
