3bac91de1293263e996be9ae2d23c31c1b7bb000
Kamil Pajdzik <pajdziupaj@gmail.com>
2022-12-11 21:16:01 -0800

    Add solution to Day 3 Part 2 of Advent of Code

--- adventofcode.com/day3.py
+++ adventofcode.com/day3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # https://adventofcode.com/2022/day/3
 
+from io import TextIOWrapper
+
 def get_priority(c: str) -> int:
     if 'a' <= c <= 'z':
         return ord(c) - ord('a') + 1
@@ -22,8 +24,26 @@ def count_priorities(input) -> int:
         result += get_priority(common_item)
     return result
 
+def count_priorities_part2(input: TextIOWrapper) -> int:
+    result = 0
+
+    while input:
+        lines = [input.readline().strip() for _ in range(3)]
+        line_sets = [set(line) for line in lines]
+        common = line_sets[0].intersection(line_sets[1], line_sets[2])
+        if not common:
+            break
+        result += get_priority(list(common)[0])
+
+    return result
+
 if __name__ == "__main__":
     input = open('day3.txt', 'r')
-    result = count_priorities(input)
-    print(result)
+    result_part1 = count_priorities(input)
+    print(f'Part1: {result_part1}')
+    input.close()
+
+    input = open('day3.txt', 'r')
+    result_part2 = count_priorities_part2(input)
+    print(f'Part2: {result_part2}')
     input.close()
