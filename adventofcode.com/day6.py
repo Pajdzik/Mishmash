70d793912a4d7bc10bdc6034cc267d68f6fe7182
Kamil Pajdzik <pajdziupaj@gmail.com>
2022-12-11 21:30:10 -0800

    Add solution to Day 6 Part 2 of Advent of Code

--- adventofcode.com/day6.py
+++ adventofcode.com/day6.py
@@ -4,17 +4,17 @@
 import io
 from collections import Counter
 
-def find_marker(file: io.TextIOWrapper) -> int:
+def find_marker(file: io.TextIOWrapper, capacity: int) -> int:
     input = file.readline()
-    last_four = list(input[0:4])
-    counter = Counter(last_four)
+    last_n = list(input[0:capacity])
+    counter = Counter(last_n)
 
     for i in range(4, len(input)):
         c = input[i]
-        if len(counter) == 4:
+        if len(counter) == capacity:
             return i
         
-        last_letter = last_four[i % 4]
+        last_letter = last_n[i % capacity]
         last_char = counter[last_letter]
 
         if last_char == 1:
@@ -22,13 +22,18 @@ def find_marker(file: io.TextIOWrapper) -> int:
         else:
             counter[last_letter] -= 1
 
-        last_four[i % 4] = c
+        last_n[i % capacity] = c
         counter[c] += 1
 
-        
+    return -1
 
 if __name__ == "__main__":
     input = open("day6.txt", "r")
-    result = find_marker(input)
-    print(result)
+    result_part1 = find_marker(input, 4)
+    print(f'Part 1: {result_part1}')
+    input.close()
+
+    input = open("day6.txt", "r")
+    result_part2 = find_marker(input, 14)
+    print(f'Part 2: {result_part2}')
     input.close()
