93e27e59e7e107ef6238598f065876e4a4ebc610
Kamil Pajdzik <pajdziupaj@gmail.com>
2022-12-11 20:31:12 -0800

    Add solution to Day 1 Part 2 of Advent of Code

--- adventofcode.com/day1.py
+++ adventofcode.com/day1.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 #https://adventofcode.com/2022/day/1
 
+from io import TextIOWrapper
+
 def find_max(input) -> int:
     max_sum = 0
     sum = 0
@@ -13,9 +15,22 @@ def find_max(input) -> int:
             sum += int(line)
 
     return max_sum
+
+def find_three_maxes(input: TextIOWrapper):
+    sums = []
+    temp_sum = 0
+    for line in input:
+        if line == "" or line == "\n":
+            sums.append(temp_sum)
+            temp_sum = 0
+        else:
+            temp_sum += int(line)
+
+    sums.sort(reverse=True)
+    return sum(sums[:3])
                 
 if __name__ == '__main__':
     input = open('day1.txt', 'r')
-    max = find_max(input)
+    max = find_three_maxes(input)
     print(max)
     input.close()
