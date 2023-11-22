b80e8f1537ef39c1cebf151100b7a3873f051d2c
Kamil Pajdzik <pajdziupaj@gmail.com>
2023-11-21 21:56:38 -0800

    Add solution to 'Defanging an IP Address' problem

---
+++ leetcode.com/defanging-an-ip-address.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+# https://leetcode.com/problems/defanging-an-ip-address/
+
+class Solution:
+    def defangIPaddr(self, address: str) -> str:
+        return address.replace(".", "[.]")
