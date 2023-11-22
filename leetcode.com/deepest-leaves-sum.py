368f50e5b52192a4a92d58973a6af75ef13e7475
Kamil Pajdzik <pajdziupaj@gmail.com>
2023-11-21 22:39:17 -0800

    Add solution to 'Deepest Leaves Sum' problem

--- leetcode.com/deepest-leaves-sum.py
+++ leetcode.com/deepest-leaves-sum.py
@@ -16,6 +16,22 @@ class TreeNode:
 
 class Solution:
     def deepestLeavesSum(self, root: Optional[TreeNode]) -> int:
+        queue = [root]
+        while queue:
+            sum = 0
+            for i in range(len(queue)):
+                node = queue.pop(0)
+                sum += node.val
+
+                if node.left:
+                    queue.append(node.left)
+
+                if node.right:
+                    queue.append(node.right)
+
+        return sum
+
+    def deepestLeavesSum_with_map(self, root: Optional[TreeNode]) -> int:
         sums = {}
         queue = [(root, 0)]
 
