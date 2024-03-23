7d40203dd58e870e5a235d6cabe3479c8f9df097
Kamil Pajdzik <pajdziupaj@gmail.com>
2024-03-22 22:43:38 -0700

    Add solution to 'Largest 3-Same-Digit Number in String' problem

---
+++ leetcode.com/largest-3-same-digit-number-in-string.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+# https://leetcode.com/problems/largest-3-same-digit-number-in-string
+
+class Solution:
+    def largestGoodInteger(self, num: str) -> str:
+        max_digit = ""
+        for i in range(0, len(num) - 2):
+            if num[i] == num[i + 1] == num[i + 2]:
+                if max_digit < num[i]:
+                    max_digit = num[i]
+
+        return max_digit
