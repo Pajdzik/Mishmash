e673df613ced0d979454fcbc6afd0da6c5cfb2da
Kamil Pajdzik <kamil@pajdzik.pl>
2024-06-11 21:08:52 -0700

    Add solution to "Sort colors" problem

--- leetcode.com/sort-colors.py
+++ leetcode.com/sort-colors.py
@@ -1,8 +1,25 @@
 #!/bin/python3
 # https://leetcode.com/problems/sort-colors/
 
+from collections import Counter
+
+
 class Solution:
+
     def sortColors(self, nums: list[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        c = Counter(nums)
+
+        i = 0
+        for color in [0, 1, 2]:
+            count = c[color]
+            for _ in range(count):
+                nums[i] = color
+                i += 1
+
+    def sortColors_2(self, nums: list[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
@@ -14,9 +31,10 @@ class Solution:
         for i in range(len(nums)):
             while count[color] <= 0:
                 color += 1
-            
+
             nums[i] = color
             count[color] -= 1
+
 
 nums = [2]
 Solution().sortColors(nums)
