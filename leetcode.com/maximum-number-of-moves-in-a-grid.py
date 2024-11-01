2a0d4b3e680031ae8b4e9434032e303df7a6fa18
Kamil Pajdzik <kamil@pajdzik.pl>
2024-10-31 21:18:46 -0700

    Add dynamic programming solution for 'Maximum Number of Moves in a Grid' problem

--- leetcode.com/maximum-number-of-moves-in-a-grid.py
+++ leetcode.com/maximum-number-of-moves-in-a-grid.py
@@ -11,6 +11,40 @@ from typing import List
 
 class Solution:
     def maxMoves(self, grid: List[List[int]]) -> int:
+        paths = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        for r in range(len(grid)):
+            paths[r][0] = 1
+
+        def check_prev_column(row: int, col: int):
+            max_path = 0
+            for row_delta in [-1, 0, 1]:
+                row_to_check = row + row_delta
+                if not 0 <= row_to_check < len(grid):
+                    continue
+                prev_col = col - 1
+                if not 0 <= prev_col:
+                    continue
+
+                curr_value = grid[row][col]
+                prev_value = grid[row_to_check][prev_col]
+                if prev_value >= curr_value or paths[row_to_check][prev_col] == 0:
+                    continue
+
+                paths[row][col] = max(
+                    paths[row][col], paths[row_to_check][prev_col] + 1
+                )
+                max_path = max(max_path, paths[row][col])
+
+            return max_path
+
+        result = 0
+        for col in range(1, len(grid[0])):
+            for row in range(len(grid)):
+                result = max(check_prev_column(row, col), result)
+
+        return max(result - 1, 0)
+
+    def maxMoves_bfs(self, grid: List[List[int]]) -> int:
         def find_next_steps(row: int, col: int) -> List[int]:
             steps = []
             for row_delta in [-1, 0, 1]:
