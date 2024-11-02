711a5d70c100974b8f26383bc2f875a4cb3c1c24
Kamil Pajdzik <kamil@pajdzik.pl>
2024-11-01 17:41:35 -0700

    Add constant memory solution for 'Circular Sentence' problem

--- leetcode.com/circular-sentence.py
+++ leetcode.com/circular-sentence.py
@@ -4,6 +4,29 @@
 
 class Solution:
     def isCircularSentence(self, sentence: str) -> bool:
+        if sentence[0] != sentence[-1]:
+            return False
+
+        index_of_last_letter = 1
+
+        while index_of_last_letter < len(sentence):
+            while (
+                index_of_last_letter + 1 < len(sentence)
+                and sentence[index_of_last_letter + 1] != " "
+            ):
+                index_of_last_letter += 1
+
+            if index_of_last_letter + 1 >= len(sentence):
+                break
+
+            if sentence[index_of_last_letter] != sentence[index_of_last_letter + 2]:
+                return False
+
+            index_of_last_letter += 2
+
+        return True
+
+    def isCircularSentence_2(self, sentence: str) -> bool:
         words = sentence.split(" ")
         for i in range(len(words)):
             if words[i - 1][-1] != words[i][0]:
@@ -19,4 +42,5 @@ if __name__ == "__main__":
         assert result == expected, f"{result=}, {expected=}, {sentence=}"
 
     test(True, "leetcode exercises sound delightful")
+    test(False, "MuFoevIXCZzrpXeRmTssj lYSW U jM")
     test(False, "Leetcode is cool")
