8c9ae853fccef17ae033f12021497871df8b390e
Kamil Pajdzik <kamil@pajdzik.pl>
2024-11-21 20:40:53 -0800

    Add two pointer solution for 'Count Pairs Whose Sum Is Less Than Target' problem

--- leetcode.com/count-pairs-whose-sum-is-less-than-target.py
+++ leetcode.com/count-pairs-whose-sum-is-less-than-target.py
@@ -8,6 +8,22 @@ class Solution:
     def countPairs(self, nums: List[int], target: int) -> int:
         result = 0
         sorted_nums = sorted(nums)
+        s = 0
+        e = len(nums) - 1
+
+        while s < e:
+            sm = sorted_nums[s] + sorted_nums[e]
+            if sm < target:
+                result += e - s
+                s += 1
+            else:
+                e -= 1
+
+        return result
+
+    def countPairs_loops(self, nums: List[int], target: int) -> int:
+        result = 0
+        sorted_nums = sorted(nums)
 
         for i in range(len(sorted_nums)):
             for j in range(i + 1, len(sorted_nums)):
