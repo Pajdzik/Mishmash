cd63b1a78b3207d6b045d78141668d295bad166f
Kamil Pajdzik <kamil@pajdzik.pl>
2024-12-01 22:04:10 -0800

    Add solution to Day 1 Part 2 of 2024 Advent of Code

--- adventofcode.com/2024/day1.ml
+++ adventofcode.com/2024/day1.ml
@@ -20,21 +20,37 @@ let print_string_list arr =
 
   print_newline ()
 
-let process_part1 lines =
-    let parse_line line  = match line with
+let foo parse_lines =
+    let split_lines line  = match line with
         | "" -> failwith "Invalid input"
         | x -> match List.filter (fun s -> s <> "") (String.split_on_char ' ' x) with
             | [x;y] -> ((int_of_string x), (int_of_string y))
             | x -> failwith "Invalid input"
       in
-    let parsed_lines = List.map parse_line lines in
+    let parsed_lines = List.map split_lines parse_lines in
 
     let first_col, second_col = List.split parsed_lines in
+    (first_col, second_col)
+
+let process_part1 lines =
+    let first_col, second_col = foo lines in
     let sorted_first_col = List.sort compare first_col in
     let sorted_second_col = List.sort compare second_col in
-
     let diffs = List.map2 (fun x y -> Int.abs(y - x)) sorted_first_col sorted_second_col in
     let sum = List.fold_left (+) 0 diffs in
     sum
 
-let () = read_lines "day1.txt" |> process_part1 |> print_int
+let process_part2 lines =
+    let first_col, second_col = foo lines in
+
+    let score n = 
+      let filtered_list = List.filter (fun x -> x == n) second_col in
+      let freq = List.length filtered_list in
+      freq * n 
+    in
+
+    let scores = List.map score first_col in
+    let sum = List.fold_left (+) 0 scores in
+    sum
+
+let () = read_lines "day1.txt" |> process_part2 |> print_int
