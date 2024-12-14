0e406e16e969fd0cb855fd4e1349bb3c52aa9b05
Kamil Pajdzik <kamil@pajdzik.pl>
2024-12-14 10:59:36 -0800

    Add solution for Day 8 Part 2 of 2024 Advent of Code

--- adventofcode.com/2024/day8.ml
+++ adventofcode.com/2024/day8.ml
@@ -70,28 +70,52 @@ let process_part1 lines =
           match (x = x', y = y') with
           | true, true -> process_position_pairs set rest
           | _, _ ->
-            let add_if_valid (x, y) set  =
-              match (x >= 0 && x < m, y >= 0 && y < n) with
-              | true, true -> TupleSet.add (x, y) set
-              | _, _ -> set
-            in
+              let add_if_valid (x, y) set =
+                match (x >= 0 && x < m, y >= 0 && y < n) with
+                | true, true -> (TupleSet.add (x, y) set, true)
+                | _, _ -> (set, false)
+              in
 
-            let diff_x = x' - x in
-            let diff_y = y' - y in
-            let first_antinode = (x - diff_x, y - diff_y) in
-            let second_antinode = (x' + diff_x, y' + diff_y) in
-            let new_set = add_if_valid first_antinode set in
-            let new_set' = add_if_valid second_antinode new_set in
-            process_position_pairs new_set' rest)
+              let rec add_until_invalid set (x, y) (dx, dy) =
+                let new_x = x + dx in
+                let new_y = y + dy in
+                let new_set, valid = add_if_valid (new_x, new_y) set in
+                match valid with
+                | true -> add_until_invalid new_set (new_x, new_y) (dx, dy)
+                | false -> new_set
+              in
+
+              let diff_x = x' - x in
+              let diff_y = y' - y in
+              let new_set = add_until_invalid set (x, y) (-diff_x, -diff_y) in
+              let new_set' =
+                add_until_invalid new_set (x', y') (diff_x, diff_y)
+              in
+              process_position_pairs new_set' rest)
     in
     process_position_pairs set position_pairs
   in
 
   let antenna_positions = find_antenna_positions CharMap.empty lines in
-  let values = CharMap.to_list antenna_positions |> List.map (fun (k, v) -> v) |> List.map (process_antenna_type TupleSet.empty) in
-  let set = List.fold_left (fun ss s -> TupleSet.union ss s) TupleSet.empty values in
-  print_tuple_set set;
-  print_newline ();
+  let antenna_positions_with_at_least_two_positions =
+    CharMap.fold
+      (fun _ v acc -> v :: acc)
+      (CharMap.filter (fun _ v -> List.length v >= 2) antenna_positions)
+      [] |> List.flatten
+  in
+  let values =
+    CharMap.to_list antenna_positions
+    |> List.map (fun (k, v) -> v)
+    |> List.map (process_antenna_type TupleSet.empty)
+  in
+  let unique_antenna_positions =
+    List.fold_left
+      (fun set el -> TupleSet.add el set)
+      TupleSet.empty antenna_positions_with_at_least_two_positions
+  in
+  let set =
+    List.fold_left (fun ss s -> TupleSet.union ss s) unique_antenna_positions values
+  in
   TupleSet.cardinal set
 
 let () =
