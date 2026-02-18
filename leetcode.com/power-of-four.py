d2b07ecb8f87cd95c2ccbf59e18a8aa433f30239
Kamil Pajdzik <kamil@pajdzik.pl>
2026-02-17 21:31:33 -0800

    Add solution to "Power of Four" problem

--- leetcode.com/power-of-four.py
+++ leetcode.com/power-of-four.py
@@ -4,9 +4,21 @@
 
 from typing import List, Optional
 
+# 4, 16, 64, 256, 1024, 4096
+# 0b100
+# 0b10000
+# 0b1000000
+# 0b100000000
 
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
+        bin_n_with_prefix = bin(n)
+        bin_n = bin_n_with_prefix[2:]
+        one, zeroes = bin_n[0], bin_n[1:]
+
+        return one == "1" and len(zeroes) & 1 == 0 and all([c == "0" for c in zeroes])
+
+    def isPowerOfFour2(self, n: int) -> bool:
         if n <= 0:
             return False
         while n > 1:
@@ -20,4 +32,4 @@ class Solution:
 
 
 if __name__ == "__main__":
-    pass
+    Solution().isPowerOfFour(64)
